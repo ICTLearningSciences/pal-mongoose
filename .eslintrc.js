d7766667226d00e582d755e5929b32f4f7196bd8
larry kirschner <larrykirschner@gmail.com>
2020-04-08 13:20:21 -0700

    fixes: lint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,19 @@
 module.exports = {
-  extends: ["eslint:recommended"],
-  env: {
-    browser: true,
-    es6: true,
-    node: true
-  },
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended"
+  ],
   parserOptions: {
-    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
-    sourceType: "module" // Allows for the use of imports
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  rules: {
+    "@typescript-eslint/explicit-function-return-type": 0,
+    "@typescript-eslint/no-explicit-any": 0,
+    "@typescript-eslint/no-this-alias": 0,
+    "@typescript-eslint/no-use-before-define": 0,
+    "@typescript-eslint/no-var-requires": 0
   }
 };
